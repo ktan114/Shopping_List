e89da63162ab25cc5ab4fc505f9bd88e352f9686
tanking <tanking@github.com>
2018-08-11 18:02:19 -0400

    Connect to mongo

--- server.js
+++ server.js
@@ -9,3 +9,9 @@ require('dotenv').config()
 
 // DB Config
 const db = require('./config/keys').mongoURL;
+
+// Connect to Mongo
+mongoose
+.connect(db)
+.then(() => console.log('MongoDB connect'))
+.catch(err => console.log(err));
