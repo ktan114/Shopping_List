0dcea050659f25e27c28161ba5e72198a8bc0453
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-08-11 18:11:50 -0400

    Changed URL to URI and added string parser to mongoose connect

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const server = express();
 
 server.use(express.json());
 
-require('dotenv').config()
+require("dotenv").config();
 
 // DB Config
-const db = require('./config/keys').mongoURL;
+const db = require("./config/keys").mongoURI;
 
 // Connect to Mongo
 mongoose
-.connect(db)
-.then(() => console.log('MongoDB connect'))
-.catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 // Connect to Server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Server started on port: ${port}`));
