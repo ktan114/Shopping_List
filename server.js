39c41b097842fbd70e1e84d3919846d7d0339f93
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-08-11 17:56:35 -0400

    Bring keys file to server

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const mongoose = require('mongoose');
 const server = express();
 
 server.use(express.json());
+
+require('dotenv').config()
+
+// DB Config
+const db = require('./config/keys').mongoURL;
