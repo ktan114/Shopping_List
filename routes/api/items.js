0e76ae736f1a87370bf3df6dfb2045b13d57cf6a
tanking <tanking@github.com>
2018-08-11 18:31:31 -0400

    Initial GET route

--- routes/api/items.js
+++ routes/api/items.js
@@ -4,4 +4,16 @@ const router = express.Router();
 // Item Model
 const Item = require('../../models/Item');
 
+// @route   GET api/items
+// @desc    Get All Items
+// @access  Public
+
+router.get('/', (req, res) => {
+    Item
+    .find()
+    .sort({ date: -1 })
+    .then(items => res.status(200).json(items))
+    .catch(err => res.status(500).json({ Error: err }))
+});
+
 module.exports = router;
