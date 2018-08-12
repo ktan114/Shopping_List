3de6e1c543e4e90c93bed44cdb51e3f8ba088be9
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-08-11 22:49:28 -0400

    Added component layout for shopping list and adding new items to state

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -4,7 +4,35 @@ import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import uuid from 'uuid';
 
 class ShoppingList extends Component {
-    
+    state = {
+        items: [
+            { id: uuid(), name: "Eggs" },
+            { id: uuid(), name: "Milk" },
+            { id: uuid(), name: "Cheese" },
+            { id: uuid(), name: "Steak" },
+        ]
+    }
+    render() {
+        const { items } = this.state;
+        return (
+            <div>
+            <Container>
+                <Button
+                  color="dark"
+                  style={{ marginBottom: '2rem' }}
+                  onClick={() => {
+                      const name = prompt('Enter Item')
+                      if (name) {
+                          this.setState(state => ({ 
+                            items: [...state.items, { id: uuid(), name }]
+                        }));
+                      }
+                  }}
+                  >Add Item</Button>
+            </Container>
+            </div>
+        );
+    }
 }
 
 export default ShoppingList;
