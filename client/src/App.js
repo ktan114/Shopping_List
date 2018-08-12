b129fb8fddd0b7a3006faf3601d37cdc37ffeeb6
tanking <tanking@github.com>
2018-08-12 00:39:34 -0400

    Removed state and connected store to ShoppingList, added proptypes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import AppNavbar from './components/AppNavbar';
-import ShoppingList from './components/ShoppingList'
-import store from './store';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
+import store from "./store";
 
 class App extends Component {
   render() {
