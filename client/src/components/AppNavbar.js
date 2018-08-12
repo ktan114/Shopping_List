6ad813421bd64c21727b0256a2cdcf47112740b9
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-08-11 20:27:33 -0400

    Set up state, toggle function, and also created Navbar layout

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -10,4 +10,32 @@ import {
   Container
 } from "reactstrap";
 
+class AppNavbar extends Component {
+    state = {
+        isOpen: false
+    }
+
+    toggle = () => {
+        this.setState({ isOpen: !this.state.isOpen })    
+    }
+
+    render() {
+        <div>
+            <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Container>
+                    <NavbarBrand href="/">ShoppingList</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={ this.state.isOpen } navbar/>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                    <NavLink href="https://github.com/ktan114">
+                                    </NavLink>
+                            </NavItem>
+                        </Nav>
+                </Container>
+            </Navbar>
+        </div>
+    }
+}
+
 export default AppNavbar;
