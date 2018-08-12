134ed55a1f77ab2490bac2409464059d9a770245
tanking <tanking@github.com>
2018-08-11 22:39:22 -0400

    Reactstrap and react-transition-group, uuid and ShoppingList class

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import uuid from 'uuid';
+
+class ShoppingList extends Component {
+    
+}
+
+export default ShoppingList;
