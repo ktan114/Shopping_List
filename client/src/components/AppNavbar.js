162daf64123e0cbfb1cb55be9a6dafece81086f7
tanking <tanking@github.com>
2018-08-11 20:41:49 -0400

    Added a closing Collapse tag

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -25,15 +25,16 @@ class AppNavbar extends Component {
             <Navbar color="dark" dark expand="sm" className="mb-5">
                 <Container>
                     <NavbarBrand href="/">ShoppingList</NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={ this.state.isOpen } navbar/>
+                    <NavbarToggler onClick={ this.toggle } />
+                    <Collapse isOpen={ this.state.isOpen } navbar >
                         <Nav className="ml-auto" navbar>
                             <NavItem>
-                                    <NavLink href="https://github.com/ktan114">
-                                    Github
-                                    </NavLink>
+                                <NavLink href="https://github.com/ktan114">
+                                Github
+                                </NavLink>
                             </NavItem>
                         </Nav>
+                    </Collapse>
                 </Container>
             </Navbar>
         </div>
