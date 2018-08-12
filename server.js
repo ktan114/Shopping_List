92d0f3d376dea97528c8d7e3aa953fa17561161c
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-08-12 02:55:52 -0400

    Add static assets if in production

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
-const items = require('./routes/api/items');
+const items = require("./routes/api/items");
 
 const server = express();
 
@@ -14,12 +15,25 @@ const db = require("./config/keys").mongoURI;
 
 // Connect to Mongo
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
 // Routes
-server.use('/api/items', items);
+server.use("/api/items", items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  server.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFILE(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Connect to Server
 const port = process.env.PORT || 5000;
